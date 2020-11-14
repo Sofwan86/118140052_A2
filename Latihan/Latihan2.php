1d2e119954a359ee0e917abd3d31599a1be27362
Sofwan86 <sofwan.hidayatnst@gmail.com>
2020-11-14 22:48:24 +0700

    mg2

---
+++ Latihan/Latihan2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>Latihan 2</title>
+    </head>
+<body>
+<?php
+ 
+$nama = array("lanirne", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat");
+sort($nama);
+for ($i=0; $i<=6; $i++)
+{
+   echo $nama[$i]." ";
+}
+?>
+</body>
+</html>
